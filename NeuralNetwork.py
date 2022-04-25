f51b00c004fc20e9d8dbb11e64fa9db7e64e726a
bliu1013 <bliu1013@unm.edu>
2022-04-25 12:16:58 -0600

    Added classification, adding files to git

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -56,9 +56,9 @@ def train(x_data, x_valid, y_data, y_valid):
     #Create model
     model = keras.models.Sequential()
     model.add(keras.layers.Flatten(input_shape=[366, 494]))
-    model.add(keras.layers.Dense(400, activation="relu"))
-    model.add(keras.layers.Dense(200, activation="relu"))
-    model.add(keras.layers.Dense(400, activation="relu"))
+    model.add(keras.layers.Dense(300, activation="relu"))
+    model.add(keras.layers.Dense(100, activation="relu"))
+    model.add(keras.layers.Dense(300, activation="relu"))
     #model.add(keras.layers.Dense(30, activation="relu"))
     #model.add(keras.layers.Dense(20, activation="relu"))
     model.add(keras.layers.Dense(6, activation="softmax"))
@@ -81,9 +81,28 @@ def train(x_data, x_valid, y_data, y_valid):
     plt.grid(True)
     plt.show()
 
+    return model
+
+
+def test(model):
+    x = []
+    n_files = 2400
+    for i in range(n_files):
+        print("Testing image", (i+1))
+        file = f"Spectrograms/{i+1}.png"
+        img = io.imread(file, as_gray=True)
+        img = img[60:426,81:575]
+        x.append(img)
+    
+    y_pred = np.argmax(model.predict(x), axis=-1)
+
+    print(y_pred)
+
 
 if __name__ == "__main__":
     x_data, x_valid, y_data, y_valid = get_data()
     #print(np.shape(x_data), y_data)
 
-    train(x_data, x_valid, y_data, y_valid)
+    model = train(x_data, x_valid, y_data, y_valid)
+
+    test(model)
