4f13b11dfb8d39fd2c343b5a79bc96db184f50aa
Ben Liu <bliu1013@unm.edu>
2022-04-26 11:31:45 -0600

    Added test indices

--- NeuralNetwork.py
+++ NeuralNetwork.py
@@ -1,23 +1,18 @@
 import sys
+import csv
 
 import sklearn
-
 import tensorflow
 
 from tensorflow import keras
-
 from skimage import io
 
-
 import numpy as np
-
 import matplotlib.pyplot as plt
 import pandas as pd
 
-
 import librosa
 import imageio
-
 
 
 # Read in spectrogram data from pngs
@@ -57,10 +52,17 @@ def train(x_data, x_valid, y_data, y_valid):
     model = keras.models.Sequential()
     model.add(keras.layers.Flatten(input_shape=[366, 494]))
     model.add(keras.layers.Dense(300, activation="relu"))
-    model.add(keras.layers.Dense(100, activation="relu"))
+    model.add(keras.layers.Dense(10, activation="relu"))
+    model.add(keras.layers.Dense(10, activation="relu"))
+    model.add(keras.layers.Dense(10, activation="relu"))
+    model.add(keras.layers.Dense(10, activation="relu"))
+    model.add(keras.layers.Dense(10, activation="relu"))
+    model.add(keras.layers.Dense(10, activation="relu"))
+    model.add(keras.layers.Dense(10, activation="relu"))
+    model.add(keras.layers.Dense(10, activation="relu"))
+    model.add(keras.layers.Dense(10, activation="relu"))
+    model.add(keras.layers.Dense(10, activation="relu"))
     model.add(keras.layers.Dense(300, activation="relu"))
-    #model.add(keras.layers.Dense(30, activation="relu"))
-    #model.add(keras.layers.Dense(20, activation="relu"))
     model.add(keras.layers.Dense(6, activation="softmax"))
 
     model.summary()
@@ -85,11 +87,19 @@ def train(x_data, x_valid, y_data, y_valid):
 
 
 def test(model):
+    #Get test example indices
+    indices = []
+    with open('test_idx.csv') as file:
+        reader = csv.reader(file)
+
+        for idx in reader:
+            if idx != 'new_id':
+                indices.append(idx)
+
     x = []
-    n_files = 2400
-    for i in range(n_files):
+    for i in range(1200):
         print("Testing image", (i+1))
-        file = f"Spectrograms/{i+1}.png"
+        file = f"test_spec/{i+1}.png"
         img = io.imread(file, as_gray=True)
         img = img[60:426,81:575]
         x.append(img)
@@ -101,8 +111,11 @@ def test(model):
 
 if __name__ == "__main__":
     x_data, x_valid, y_data, y_valid = get_data()
-    #print(np.shape(x_data), y_data)
 
     model = train(x_data, x_valid, y_data, y_valid)
 
-    test(model)
+    #Prompt to continue to testing
+    if input("Continue? (y/n)") == 'y':
+        test(model)
+
+    
