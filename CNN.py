e5164c053b291aa85f4a63ec1ab68e011f02da5c
LoboBAT <bockj1998@unm.edu>
2022-05-01 14:19:23 -0600

    mod CNN

--- CNN.py
+++ CNN.py
@@ -135,7 +135,8 @@ def test(model):
 if __name__ == "__main__":
     x_data, x_valid, y_data, y_valid = get_data()
     #print(np.shape(x_data), y_data)
-
+    x_data = x_data.reshape(-1, 28, 28, 1)
+    x_valid = x_valid.reshape(-1, 28, 28, 1)
     model = train(x_data, x_valid, y_data, y_valid)
 
     test(model)
