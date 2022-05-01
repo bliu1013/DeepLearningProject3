37c0efb88930573ad83a384f6ee340f0eb2fc5f9
LoboBAT <bockj1998@unm.edu>
2022-05-01 14:01:49 -0600

    mod CNN

--- CNN.py
+++ CNN.py
@@ -14,6 +14,7 @@ import tensorflow as tf
 from tensorflow.keras import datasets, layers, models
 import matplotlib.pyplot as plt
 from tensorflow import keras
+import random
 
 from skimage import io
 
@@ -32,31 +33,42 @@ import imageio
 
 # Read in spectrogram data from pngs
 def get_data():
-    n_files = 2400
-
     x = []
     x_val = []
     y = []
     y_val = []
 
-    class_num = 0
-    for i in range(n_files):
-        print("Reading image", (i+1))
-        file = f"Spectrograms/{i+1}.png"
+    #Shuffle the order of data
+    specs = np.linspace(1, 2400, 2400, dtype=int)
+    random.shuffle(specs)
+    for num, i in enumerate(specs):
+        print("Reading image", (num))
+        file = f"Spectrograms/{i}.png"
         img = io.imread(file, as_gray=True)
 
         #Get rid of extra pixels
         img = img[60:426,81:575]
+
+        if i <= 401:
+            class_num = 0
+        elif i <= 801:
+            class_num = 1
+        elif i <= 1201:
+            class_num = 2
+        elif i <= 1601:
+            class_num = 3
+        elif i <= 2001:
+            class_num = 4
+        else:
+            class_num = 5    
+
         #Split data for validation
-        if i % 4 == 0:
+        if i % 5 == 0:
             x_val.append(img)
             y_val.append(class_num)
         else:
             x.append(img)
             y.append(class_num)
-
-        if (i+1) % (400) == 0:
-            class_num += 1
 
     return x, x_val, y, y_val
 
