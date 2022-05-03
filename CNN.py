dfab7f0a96838b9a1eddc7f7319e1017918ca129
LoboBAT <bockj1998@unm.edu>
2022-05-02 22:05:15 -0600

    mod CNN

--- CNN.py
+++ CNN.py
@@ -6,7 +6,14 @@ Created on Tue Apr 26 22:53:49 2022
 @author: jared
 """
 import sys
-
+import numpy as np
+import matplotlib.pyplot as plt
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import tensorflow as tf
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
+from keras.preprocessing import image
+from tensorflow.keras.models import load_model
 import sklearn
 from sklearn import preprocessing
 import tensorflow as tf
@@ -75,6 +82,40 @@ def get_data():
 
 def train(x_data, x_valid, y_data, y_valid):
     #Create model
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    valid_datagen = ImageDataGenerator(rescale=1./255)
+    test_datagen = ImageDataGenerator(rescale=1./255)
+    
+    train_generator = train_datagen.flow_from_directory(
+    directory=r"Data/train/",
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=32,
+    class_mode="categorical",
+    shuffle=True,
+    seed=42
+    )
+
+    valid_generator = valid_datagen.flow_from_directory(
+    directory=r"Data/validation/",
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=32,
+    class_mode="categorical",
+    shuffle=True,
+    seed=42
+    )
+
+    test_generator = test_datagen.flow_from_directory(
+    directory=r"Data/test/",
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=32,
+    class_mode="categorical",
+    shuffle=True,
+    seed=42
+    )
+
 
     model = keras.models.Sequential()
 
@@ -99,7 +140,22 @@ def train(x_data, x_valid, y_data, y_valid):
     model.compile(loss="sparse_categorical_crossentropy",
                   optimizer="adam",
                   metrics=["accuracy"])  
-
+    
+    model.fit(train_generator,batch_size=(32),epochs=30)
+    
+    accuracy = model.evaluate(validation_generator)
+    print('n', 'Test_Accuracy:-', accuracy[1])
+    pred = model.predict(validation_generator)
+    y_pred = np.argmax(pred, axis=1)
+    y_true = np.argmax(pred, axis=1)
+    print('confusion matrix')
+    print(confusion_matrix(y_true, y_pred))
+    #confusion matrix
+    f, ax = plt.subplots(figsize=(8,5))
+    sns.heatmap(confusion_matrix(y_true, y_pred), annot=True, fmt=".0f", ax=ax)
+    plt.xlabel("y_pred")
+    plt.ylabel("y_true")
+    plt.show()
     #Convert all lists to nparrays
     x_data = np.asarray(x_data)
     x_valid = np.asarray(x_valid)
@@ -145,17 +201,92 @@ def test(model):
             
 
 if __name__ == "__main__":
-    x_data, x_valid, y_data, y_valid = get_data()
-    #print(np.shape(x_data), y_data)
-    x_data = np.expand_dims(x_data, axis=-1)
-    x_valid = np.expand_dims(x_valid, axis=-1)
-    model = train(x_data, x_valid, y_data, y_valid)
+    # x_data, x_valid, y_data, y_valid = get_data()
+    # #print(np.shape(x_data), y_data)
+    # x_data = np.expand_dims(x_data, axis=-1)
+    # x_valid = np.expand_dims(x_valid, axis=-1)
+    # model = train(x_data, x_valid, y_data, y_valid)
 
-    while True:
-        text = input("Continue to test? (y/n)\n")
+    # while True:
+    #     text = input("Continue to test? (y/n)\n")
 
-        if text == 'y':
-            test(model)
-            break
-        elif text == 'n':
-            break
+    #     if text == 'y':
+    #         test(model)
+    #         break
+    #     elif text == 'n':
+    #         break
+###################
+    train_datagen = ImageDataGenerator(rescale=1./255)
+    valid_datagen = ImageDataGenerator(rescale=1./255)
+    test_datagen = ImageDataGenerator(rescale=1./255)
+
+    train_generator = train_datagen.flow_from_directory(
+    directory=r"Data/train/",
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=32,
+    class_mode="categorical",
+    shuffle=True,
+    seed=42
+    )
+
+    valid_generator = valid_datagen.flow_from_directory(
+    directory=r"Data/validation/",
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=32,
+    class_mode="categorical",
+    shuffle=True,
+    seed=42
+    )
+
+    test_generator = test_datagen.flow_from_directory(
+    directory=r"Data/test/",
+    target_size=(224, 224),
+    color_mode="rgb",
+    batch_size=32,
+    class_mode="categorical",
+    shuffle=True,
+    seed=42
+    )
+
+
+    model = keras.models.Sequential()
+
+    model.add(layers.Conv2D(32, (5, 5),padding ="same", activation='relu', input_shape=(366, 494, 1)))
+    model.add(layers.AveragePooling2D((2, 2)))
+    #model.add(layers.Dropout(.2))
+    model.add(layers.Conv2D(64, (3, 3),padding ="same", activation='relu'))
+    model.add(layers.AveragePooling2D((2, 2)))
+    #model.add(layers.Dropout(.2))
+    model.add(layers.Conv2D(64, (3, 3),padding ="same", activation='relu'))
+
+
+
+    model.add(layers.Flatten())
+    model.add(layers.Dense(64, activation='relu'))
+    #model.add(layers.Dropout(.2))
+    model.add(layers.Dense(64, activation='relu'))
+
+    model.add(layers.Dense(6, activation='softmax'))
+    model.summary()
+
+    model.compile(loss="sparse_categorical_crossentropy",
+                  optimizer="adam",
+                  metrics=["accuracy"])  
+    
+    model.fit(train_generator,batch_size=(32),epochs=30)
+    
+    accuracy = model.evaluate(valid_generator)
+    print('n', 'Test_Accuracy:-', accuracy[1])
+    pred = model.predict(valid_generator)
+    y_pred = np.argmax(pred, axis=1)
+    y_true = np.argmax(pred, axis=1)
+    print('confusion matrix')
+    print(confusion_matrix(y_true, y_pred))
+    #confusion matrix
+    f, ax = plt.subplots(figsize=(8,5))
+    sns.heatmap(confusion_matrix(y_true, y_pred), annot=True, fmt=".0f", ax=ax)
+    plt.xlabel("y_pred")
+    plt.ylabel("y_true")
+    plt.show()
