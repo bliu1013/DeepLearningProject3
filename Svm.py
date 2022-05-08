9835e2e9161c5d796afce0d0ee9652def97bfc91
Danyelle Loffredo <danyellemontalvo@gmail.com>
2022-05-08 16:07:34 -0600

    ReadMe and SVM

--- Svm.py
+++ Svm.py
@@ -1,16 +1,7 @@
-import sys
-import csv
-import tensorflow
-import imageio
 import random
-import numpy as np
 import matplotlib.pyplot as plt
 from sklearn import svm
-import csv
-from sklearn.datasets import make_blobs
 
-
-from tensorflow import keras
 from skimage import io
 from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 import sklearn as skl
@@ -20,11 +11,10 @@ import librosa.display
 from joblib import Parallel,delayed
 
 import numpy as np
-import matplotlib.pyplot as plt
 import pandas as pd
 
 
-#computeMFCC(Sings,"/home/jared/Downloads/project3/train/",files,2401)
+#Section of code was adapted from https://librosa.org/doc/main/generated/librosa.feature.mfcc.html
 def computeMFCC(limit):
     """
     Function to compute ceptral coefficients
@@ -132,6 +122,7 @@ def get_data_validation():
     #x and y are training data x_val, y_val are validation datasets
     return x, x_val, y, y_val
 
+#Section of code used from the SVM workbook on UNM Learn
 def svm_train(x, y):
     """
     Function used to train the SVM
@@ -206,47 +197,46 @@ def validation_testing(y_predict, y_actual):
 
 
 if __name__ == "__main__":
-    #Code to begin training and testing the svm model with MFCC data
-    #Mfcc, classes, somevalue, othervalue = computeMFCC(2400) #2400
-    #Mfcc, ids = computeMFCC(2400)
-    #clf = svm_train(Mfcc, classes)
-    #validation_x, identifiers = computeMFCC_test(1200) #1200
-    #identifiers = identifiers[:1200]
-    #new_classes = clf.predict(validation_x)
-    #classifier(identifiers, new_classes)
-    #accuracy = validation_testing(new_classes, validation_y)
-    #print(accuracy)
-    #Code to begin 5-point cross validation
-    x, y, x_val, y_val = computeMFCC(2400) #2400
-    #x, x_val, y, y_val = get_data_validation()
-    accuracies = []
-    for i in range(5):
-        fifth = len(x)//5
-        x_val = x[i*fifth:(i+1)*fifth]
-        y_val = y[i*fifth:(i+1)*fifth]
-        x_train = x[:i*fifth] + x[(i+1)*fifth:]
-        y_train = y[:i*fifth] + y[(i+1)*fifth:]
-        clf = svm_train(x_train, y_train)
-        classes = clf.predict(x_val)
-        print("calculating accuracies for "+str(i+1))
-        accuracies.append(validation_testing(classes, y_val))
-        if i == 1:
-            spec_array = classes
-            actual = y_val
-            predicted = spec_array
-            disp_label = [0,1, 2, 3, 4, 5]
-            matrix = confusion_matrix(actual, predicted, labels=[0, 1, 2, 3, 4, 5])
-            print(matrix)
-            disp = ConfusionMatrixDisplay(confusion_matrix= matrix, display_labels=disp_label)
-            disp.plot()
-            plt.show()
 
-    #print(average(accuracies))
-    #clf = svm_train(x, y)
-    #first_class = clf.predict(x_val)
-    #Function to begin valication accuracy
-    #validation_testing(first_class, y_val)
-    #Code to begin classification of testing dataset for Spectrogram
+    #Code to begin training and testing the svm model with MFCC data
+    Mfcc, classes, somevalue, othervalue = computeMFCC(2400) #2400
+    #Mfcc, ids = computeMFCC(2400)
+    clf = svm_train(Mfcc, classes)
+    validation_x, identifiers = computeMFCC_test(1200) #1200
+    identifiers = identifiers[:1200]
+    new_classes = clf.predict(validation_x)
+    classifier(identifiers, new_classes)
+
+###############################################################
+    #Code to begin 5-point cross validation
+    #x, y, x_val, y_val = computeMFCC(2400) #2400
+    #x, x_val, y, y_val = get_data_validation()
+    #accuracies = []
+    #for i in range(5):
+    #    fifth = len(x)//5
+    #    x_val = x[i*fifth:(i+1)*fifth]
+    #    y_val = y[i*fifth:(i+1)*fifth]
+    #    x_train = x[:i*fifth] + x[(i+1)*fifth:]
+    #    y_train = y[:i*fifth] + y[(i+1)*fifth:]
+    #    clf = svm_train(x_train, y_train)
+    #    classes = clf.predict(x_val)
+    #    print("calculating accuracies for "+str(i+1))
+    #    accuracies.append(validation_testing(classes, y_val))
+    #    print(accuracies)
+    #    if i == 1:
+    # Code to print confusion Matrix
+    #        spec_array = classes
+    #        actual = y_val
+    #        predicted = spec_array
+    #        disp_label = [0,1, 2, 3, 4, 5]
+    #        matrix = confusion_matrix(actual, predicted, labels=[0, 1, 2, 3, 4, 5])
+    #        print(matrix)
+    #        disp = ConfusionMatrixDisplay(confusion_matrix= matrix, display_labels=disp_label)
+    #        disp.plot()
+    #        plt.show()
+
+#######################################################################
+    #Code to begin classification of testing dataset for Spectrogram data
     #x, x_val, y, y_val = get_data_validation()
     #x_val, y_val = get_data_test()
     #clf = svm_train(x, y)
