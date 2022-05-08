99db95d4e8026f689da4eff25fabe2b856400d55
LoboBAT <bockj1998@unm.edu>
2022-05-07 22:26:16 -0600

    Adjusting

--- CNN.py
+++ CNN.py
@@ -80,7 +80,7 @@ if __name__ == "__main__":
     model = keras.models.Sequential()
 
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(480, 480, 3)))
-    model.add(layers.BatchNormalization())
+    model.add(layers.MaxPool2D())
     model.add(layers.Flatten())
 
     model.add(layers.Dense(6, activation='softmax'))
