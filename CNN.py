a47c4db2398e13899c45fae3d43bb26471c3d4de
LoboBAT <bockj1998@unm.edu>
2022-05-08 03:13:19 -0600

    Adjusting

--- CNN.py
+++ CNN.py
@@ -79,18 +79,16 @@ if __name__ == "__main__":
 
     model = keras.models.Sequential()
 
-    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(480, 480, 3),activity_regularizer=(tf.keras.regularizers.L2(1e-1))))
+    model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(480, 480, 3),kernel_regularizer=(tf.keras.regularizers.L2(1e-3)),activity_regularizer=(tf.keras.regularizers.L2(1e-1))))
     model.add(layers.MaxPool2D())
-    model.add(layers.Conv2D(64, (3, 3), activation='relu',activity_regularizer=(tf.keras.regularizers.L2(1e-1))))
-    model.add(layers.BatchNormalization())
+    model.add(layers.Conv2D(64, (3, 3), activation='relu',kernel_regularizer=(tf.keras.regularizers.L2(1e-3)),activity_regularizer=(tf.keras.regularizers.L2(1e-3))))
     model.add(layers.MaxPool2D())
     model.add(layers.Flatten())
-
     model.add(layers.Dense(6, activation='softmax'))
     #model = make((480,480))
     model.compile(loss="categorical_crossentropy",
-                  optimizer=tf.keras.optimizers.SGD(
-    learning_rate=1e-11),
+                  optimizer=tf.keras.optimizers.Adam(
+    learning_rate=1e-8),
                   metrics=["accuracy"])   
     
     
