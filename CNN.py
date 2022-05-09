dcfeddfc5b469a1932bc9943987162c717b1221f
LoboBAT <bockj1998@unm.edu>
2022-05-09 02:12:06 -0600

    2 consec conv2d

--- CNN.py
+++ CNN.py
@@ -26,40 +26,13 @@ import numpy as np
 
 import matplotlib.pyplot as plt
 import pandas as pd
-
-
- 
- 
-def make(input_shapes):
-    inputs = keras.Input(shape=input_shapes)
-    x = layers.Rescaling(1./255, offset=0.0)(inputs)   
-    x = layers.Conv2D(32,3,strides=2,padding = "same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-
-    x = layers.Conv2D(64,3,strides=2,padding = "same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-    
-    x = layers.Conv2D(128, 3, padding="same")(x)
-    x = layers.BatchNormalization()(x)
-    x = layers.Activation("relu")(x)
-    
-    x = layers.GlobalAveragePooling2D()(x)
-    x=layers.Dropout(.2)(x)
-    outputs = layers.Dense(6,activation = "softmax")(x)
-    return keras.model(inputs,outputs)
-
-    
-
-
             
-
 if __name__ == "__main__":
 
     train_datagen = ImageDataGenerator(rescale = 1/255.0,validation_split=0.2)
     #valid_datagen = ImageDataGenerator(rescale = 1/255.0)
-    
+#Create training data set from Data directory
+
     train_ds = train_datagen.flow_from_directory(
     directory="Data/training",
     target_size = (480,480),
@@ -68,7 +41,7 @@ if __name__ == "__main__":
     subset='training',
     shuffle=(True)
     )
-
+#Create validation data set from Data directory
     valid_ds = train_datagen.flow_from_directory(
     directory="Data/training",
     target_size = (480,480),
@@ -77,17 +50,12 @@ if __name__ == "__main__":
     subset='validation'
     )
 
+#Model creation starting with relu activation
     model = keras.models.Sequential()
-
     model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(480, 480, 3)))  
-    model.add(layers.Conv2D(32, (3, 3), activation='relu'))    
-
-    model.add(layers.MaxPool2D())
-    #model.add(layers.Conv2D(32, (3, 3), activation='relu'))    
     model.add(layers.MaxPool2D())
     model.add(layers.Flatten())
     model.add(layers.Dense(6, activation='softmax'))
-    #model = make((480,480))
     model.compile(loss="categorical_crossentropy",
                   optimizer=tf.keras.optimizers.SGD(
     learning_rate=1e-8),
@@ -95,35 +63,9 @@ if __name__ == "__main__":
     
     
     model.summary()
-    
+    #fit model
     history = model.fit(train_ds, epochs=30, validation_data=valid_ds)    
-    
-    img = keras.preprocessing.image.load_img(
-    "test_spec/1.png", target_size=(480,480)
-    )
-    img_array = keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
-    predictions = model.predict(img_array)
-    score = predictions[0]
-    print(score)
-   
-    img = keras.preprocessing.image.load_img(
-    "test_spec/105.png", target_size=(480,480)
-    )
-    img_array = keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
-    predictions = model.predict(img_array)
-    score = predictions[0]
-    print(score)
-    
-    img = keras.preprocessing.image.load_img(
-    "test_spec/700.png", target_size=(480,480)
-    )
-    img_array = keras.preprocessing.image.img_to_array(img)
-    img_array = tf.expand_dims(img_array, 0)  # Create batch axis
-    predictions = model.predict(img_array)
-    score = predictions[0]
-    print(score)
+
     
     pd.DataFrame(history.history).plot(figsize=(8, 5))
     plt.grid(True)
--- Representation.py
+++ Representation.py
@@ -17,10 +17,15 @@ from scipy.stats import loguniform
 from sklearn.model_selection import RepeatedStratifiedKFold
 from sklearn.model_selection import RandomizedSearchCV
 import seaborn as sns
+'''
+####################################################################
+path_to_audio: is path to mp3 files
 
+files_csv_path: CSV containing mp3 file name numbers
 
-
-files = np.genfromtxt("/home/jared/Videos/project3/train.csv", delimiter = ',',dtype = str)
+####################################################################
+'''
+#files = np.genfromtxt("/home/jared/Videos/project3/train.csv", delimiter = ',',dtype = str)
 test_files =  np.genfromtxt("/home/jared/Videos/project3/test_idx.csv", delimiter = ',',dtype = str)
 Xs1 = []
 Xs2 = []
@@ -33,7 +38,7 @@ start, end = 0, 27
 Compute the MFCC of .mp3 file, then take SVD and create matrix of 21 largest singular values
 Mx21 where M is the number of audio files processed.
 """
-def computeMFCC(Sings,path_to_audio,files,limit,files_csv_path):
+def computeMFCC(Sings,path_to_audio,limit,files_csv_path):
     files = np.genfromtxt(files_csv_path, delimiter = ',',dtype = str)
     for i in range(1,limit):
         if (len(files[0]) ==2):
@@ -56,8 +61,11 @@ def computeMFCC(Sings,path_to_audio,files,limit,files_csv_path):
         Sings.append(B)
     return Sings
 
-
-def computeMFCC_Parallel(i,path_to_audio,files,files_csv_path):
+"""
+Compute MFCC in a way that is friendly to parallel processing call to increase 
+speed
+"""
+def computeMFCC_Parallel(i,path_to_audio,files_csv_path):
         files = np.genfromtxt(files_csv_path, delimiter = ',',dtype = str)
 
         if (len(files[0]) ==2):
@@ -69,7 +77,9 @@ def computeMFCC_Parallel(i,path_to_audio,files,files_csv_path):
         mel = librosa.feature.melspectrogram(sr=sr, S=stft**2)
         mfcc = librosa.feature.mfcc(S=librosa.power_to_db(mel))
         return mfcc
-
+"""
+Create and save spectogram of audio file. Written to be parallel friendly
+"""
 def computeSpec(i,Xs,Path_to_audio_files,files_csv_path):
     files = np.genfromtxt(files_csv_path, delimiter = ',',dtype = str)
 
@@ -77,13 +87,9 @@ def computeSpec(i,Xs,Path_to_audio_files,files_csv_path):
     filename = Path_to_audio_files + files[i][0]+".mp3"
         #All songs seem to be at least 29s long
     x, sr = librosa.load(filename, sr=None, mono=True,duration=29)
-    #librosa.display.waveplot(Xs[i], SRs[i], alpha=0.5);
-    #plt.vlines([start, end], -1, 1)
-    #start = len(x) // 2
+
     plt.figure()
-    # plt.plot(Xs[i][start:start+2000])
-    # plt.ylim((-1, 1));
-    # plt.show()
+
     stft = np.abs(librosa.stft(x, n_fft=2048, hop_length=512))
     mel = librosa.feature.melspectrogram(sr=sr, S=stft**2)
     log_mel = librosa.amplitude_to_db(mel)
@@ -94,71 +100,42 @@ def computeSpec(i,Xs,Path_to_audio_files,files_csv_path):
     plt.show()
 
 
-def compSVD_dft(Xs,Path_to_audio_files,files_csv_path):
-    files = np.genfromtxt(files_csv_path, delimiter = ',',dtype = str)
+if __name__ == "__main__":
 
-    for i in range(1,2401):
-        if(len(files[i,0])==6):
-            filename = Path_to_audio_files + files[i,0]+".mp3"
-        else:
-            filename = Path_to_audio_files +files[i,0]+".mp3"
-        x, sr = librosa.load(filename, sr=None, mono=True,duration=29)
-        #Xs.append(x.tolist())
-        print(x.shape)
-        #dft = np.abs(librosa.stft(x))
-        #dft = librosa.amplitude_to_db(dft, ref=np.max)
-        #print(dft.shape)
-        #plt.plot(dft)
-        svd = skl.decomposition.TruncatedSVD(n_components = 50).fit(x.reshape(1,-1))
-        print("SVD",svd.singular_values_.T)
-        print(svd.singular_values_.T.flatten())
-        Xs.append(svd.singular_values_)
-    return Xs
 
-def comp_for_SVD(Xs, genre,Path_to_audio_files,files_csv_path):
-    files = np.genfromtxt(files_csv_path, delimiter = ',',dtype = str)
+    p_to_audio = input("Path to audio files: \n")
+    p_to_csv = input("Path to test or training csv: \n")
+    #Only run the Parallel job once. Thses will consume a lot of memory and aren't necessary to run unles you make chages
 
-    for i in range(1,401):
-        #if(files[i][1]==str(genre)):
-            print(files[i][0])
-            
-            filename = Path_to_audio_files + files[i,0]+".mp3"
-            x, sr = librosa.load(filename, sr=None, mono=True,duration=15)
-            Xs.append(x.tolist())
-            print(len(Xs))
-    svd = skl.decomposition.TruncatedSVD(n_components = 5).fit(Xs)
-    print("SVD",svd.singular_values_)
-    return Xs
-
-#Only run the Parallel job once.
-#Parallel(n_jobs = -1)(delayed(computeSpec)(i,Xs1) for i in range(1,2401))
-#computeSpec(4, Xs1)
-computeMFCC(Sings,"/home/jared/Downloads/project3/train/",files,2401,"/home/jared/Videos/project3/train.csv")
+    #Parallel(n_jobs = -1)(delayed(computeSpec)(i,Xs1,p_to_audio,p_to_csv) for i in range(1,2401))
+    computeMFCC(Sings,p_to_audio,2401,p_to_csv)
 
 #PARALLEL CALL TO SPEED UP IF NEEDING MFCC AS INPUT
-#Parallel(n_jobs = -1)(delayed(computeMFCC_Parallel)(i,"/home/jared/Downloads/project3/train/") for i in range(1,1201))
-
-Sings = np.array(Sings)
-newlist = [Sings[x][-1] for x in range(len(Sings))]
-B = np.delete(Sings, -1, axis=1)
-print(newlist)
-skl.preprocessing.normalize(B, norm='l2')
-
-x_train, x_test, y_train, y_test = train_test_split(B, newlist, test_size=0.2, shuffle=True)
-#Try w/150ish iter to improve acc
-logReg = skl.linear_model.LogisticRegression(multi_class='multinomial', solver='newton-cg',max_iter = 119)
-logReg.fit(x_train,y_train)
-
-score = logReg.score(x_test, y_test)
-y_pred = logReg.predict(x_test)
-cf= sklearn.metrics.confusion_matrix(y_test, y_pred)
-sns.heatmap(cf,annot=True)
-print(score)
-
-while True:
+    #Parallel(n_jobs = -1)(delayed(computeMFCC_Parallel)(i,p_to_audio) for i in range(1,1201))
+    Sings = np.array(Sings)
+    newlist = [Sings[x][-1] for x in range(len(Sings))]
+    B = np.delete(Sings, -1, axis=1)
+    print(newlist)
+    skl.preprocessing.normalize(B, norm='l2')
+    
+    x_train, x_test, y_train, y_test = train_test_split(B, newlist, test_size=0.2, shuffle=True)
+    #Try w/150ish iter to improve acc
+    logReg = skl.linear_model.LogisticRegression(multi_class='multinomial', solver='newton-cg',max_iter = 119)
+    logReg.fit(x_train,y_train)
+    #Score model accuracy for validation data
+    score = logReg.score(x_test, y_test)
+    y_pred = logReg.predict(x_test)
+#Conf. matrix
+    cf= sklearn.metrics.confusion_matrix(y_test, y_pred)
+    sns.heatmap(cf,annot=True)
+    print(score)
+        #If yes, run prediction on test data
+    while True:
         text = input("Continue to test? (y/n)\n")
         if text == 'y':
-            computeMFCC(Sings2,"/home/jared/Downloads/project3/test/",test_files,1201)
+            p_to_audio2 = input("Path to audio files: \n")
+            p_to_csv2 = input("Path to test or training csv: \n")
+            computeMFCC(Sings2,p_to_audio2,1201,p_to_csv2)
             Sings2 = np.array(Sings2)
             skl.preprocessing.normalize(Sings2, norm='l2')
             predictions = logReg.predict(Sings2)
